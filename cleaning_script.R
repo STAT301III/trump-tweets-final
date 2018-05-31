d9b61448d5d87320b049d5e42abe0027c65a230e
tiffjeung <jeung.tiffany@gmail.com>
2018-05-31 13:27:20 -0500

    Cleaning script currently not function - Twitter experiencing API issues.

--- cleaning_script.R
+++ cleaning_script.R
@@ -17,14 +17,14 @@ trump_tweets <- trump_tweets %>%
   select(-status_id, -user_id, -reply_to_status_id, -reply_to_user_id, -is_quote, -symbols, -media_t.co, -media_expanded_url, -ext_media_url, -ext_media_t.co, -ext_media_expanded_url, -ext_media_type, -mentions_user_id, -lang, -quoted_status_id, -retweet_status_id, -retweet_text, -place_url, -place_type, -country, -bbox_coords, -coords_coords, -source, -geo_coords) %>% 
   mutate(created_at = mdy_hm(created_at))
 
-# Tweet time range
+# Tweet time range of original Trump tweets dataset
 # 2009-05-04 18:54:00
 # 2017-08-10, 10:54:00
 trump_tweets %>% 
   arrange(created_at)
 
 
-## -- Setup with Twitter API using rtweet -- ##
+## -- Setup  Twitter API using rtweet to extract tweets -- ##
 
 # Reference name of twitter app
 app_name <- "tj_twitter_app"
@@ -48,10 +48,12 @@ cat(env_var, file = file.path(path.expand("~"), ".Renviron"),
 
 ## -- Acquire Tweet data using rtweet -- ##
 
+tweets <- get_timeline("tiffjeung", n = 10)
+
 # Acquire most recent 3200 statuses posted
 trump_newest <- get_timeline("realDonaldTrump", n = 3200)
 
-# Remove extra variables
+# Remove extra variables to match first Trump dataset
 trump_newest <- trump_newest %>%
   select(-status_id, -user_id, -reply_to_status_id, -reply_to_user_id, -is_quote, -symbols, -media_t.co, -media_expanded_url, -ext_media_url, -ext_media_t.co, -ext_media_expanded_url, -ext_media_type, -mentions_user_id, -lang, -quoted_status_id, -retweet_status_id, -retweet_text, -place_url, -place_type, -country, -bbox_coords, -coords_coords, -source, -urls_url, -urls_t.co, -urls_expanded_url, -place_full_name, -geo_coords)
 
@@ -66,7 +68,7 @@ trump_newest %>%
   arrange(desc(created_at))
 
 # Write to csv for future reference (as get_timeline will get different tweets every time, and eventually the trump_newest dataset will no longer overlap with the original dataset)
-write_as_csv(trump_newest, "data/unprocessed/trump_may4.csv")
+#write_as_csv(trump_newest, "data/unprocessed/trump_may4.csv")
 
 # Check if variables match
 names(trump_newest)
@@ -87,7 +89,7 @@ names(trump_all)
 # Exclamation points
 trump_all <- trump_all %>% 
   mutate(exclam = str_detect(text, "!"),
-         exlam_count = str_count(text, "!"))
+         exclam_count = str_count(text, "!"))
 
 # Url embedded?
 trump_all <- trump_all %>% 
@@ -97,9 +99,15 @@ trump_all <- trump_all %>%
 trump_all <- trump_all %>% 
   mutate(all_caps = str_extract_all(text, "[A-Z]{2,}"))
 
+trump_all <- trump_all %>% 
+  mutate(all_caps_count = sapply(all_caps, length))
+
 # First letter uppercase words
 trump_all <- trump_all %>% 
   mutate(uppercase_first = str_extract_all(text, "[A-Z][a-z]+"))
+
+trump_all <- trump_all %>% 
+  mutate(uppercase_first_count = sapply(uppercase_first, length))
 
 
 ## -- Save Trump tweet dataset -- ##
@@ -109,7 +117,7 @@ set.seed(5)
 
 # Set aside performance set (20 percent)
 train_trump <- trump_all %>% sample_frac(0.8)
-test_trump <- trump_all %>% setdiff(train_trump)
+final_test_trump <- trump_all %>% setdiff(train_trump)
 
 # Read tidied trump datasets for use
 saveRDS(trump_all, "data/processed/trump_all.rds")
@@ -118,8 +126,8 @@ write_as_csv(trump_all, "data/processed/trump_all.csv")
 saveRDS(train_trump, "data/processed/train_trump.rds")
 write_as_csv(train_trump, "data/processed/train_trump.csv")
 
-saveRDS(test_trump, "data/processed/test_trump.rds")
-write_as_csv(test_trump, "data/processed/test_trump.csv")
+saveRDS(final_test_trump, "data/processed/final_test_trump.rds")
+write_as_csv(final_test_trump, "data/processed/final_test_trump.csv")
 
 
 ## -- Fox and Friends Tweets -- ##
@@ -156,9 +164,15 @@ foxandfriends <- foxandfriends %>%
 foxandfriends <- foxandfriends %>% 
   mutate(all_caps = str_extract_all(text, "[A-Z]{2,}"))
 
+foxandfriends <- foxandfriends %>% 
+  mutate(all_caps_count = sapply(all_caps, length))
+
 # First letter uppercase words
 foxandfriends <- foxandfriends %>% 
   mutate(uppercase_first = str_extract_all(text, "[A-Z][a-z]+"))
+
+foxandfriends <- foxandfriends %>% 
+  mutate(uppercase_first_count = sapply(uppercase_first, length))
 
 ## -- Store Fox and Friends tweets for EDA -- ##
 
